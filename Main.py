a27b132315df7f35625d23d9b6c6e970c99079ae
kiwiwik <49052768+kiwiwik@users.noreply.github.com>
2019-12-10 17:51:20 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
 
-from algorithms import prims
-from Functions.graph_operations import graph_cost
+from Algorithms.prims_algorithm import prims
+#from Functions.graph_operations import graph_cost
 
-text = input("Please provide a graph to run the TSP on ")
 
-text = input("PLease provide a starting vertex ")
-T = prims(G, v)
-print('Optimal Tree is {T}')
+#text = input("Please provide a graph to run the TSP on ")
+
+#text = input("PLease provide a starting vertex ")
+T = prims
+print(f'Optimal Tree:', prims)
 
 print('')
 
-print(f'Optimal cost: {graph_cost(T)}')
+#print(f'Optimal cost: {graph_cost(G)}')
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from Functions.reading_writing_functions import get_graph
-from Functions.graph_operations import incident_edges, cost, graph_cost
+from Functions.graph_operations import incident_edges, cost, graph_cost, min_cost_incident_edge
+from Algorithms.prims_algorithm import prims
 
 G = get_graph('G1.txt')
     
@@ -13,16 +14,21 @@ T = ([3, 1], [(3, 1),(1, 4)] )
 
 print(f' T = {T}')
 print('')
+
+print("Edges:")
 for e in G[1]:
     print(e)
 
-#print(incident_edges(G, T))
+
+print("Incident Edges:", incident_edges(G, T))
 
 
-# print(f'The cost of edge (1,3) is {cost(G, (1,3))}')
-    
 for e in incident_edges(G, T):
     print(f'Edge {e} with cost {cost(G, e)}')
     
-print(graph_cost)
+
+#print(f'The cost of edge (1,3) is {cost(G, (1,3))}')
+print("Minimum cost incident edge:", min_cost_incident_edge(G,T))
+print(f'Total weight: {graph_cost(G)}')
+
 
